675911e2cc15883af31189593aa7df6378d08bd8
Sanya Oluwadare <sanyaodare@gmail.com>
2021-07-07 23:00:52 +0100

    itirate through images

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
+const gallery = document.getElementById('gallery');
+const popup = document.getElementById('popup');
+const selectedImage = document.getElementById('selectedImage');
+const imageIndexes = [1,2,3,4,5,6,7,8,9,10];
+const selectedIndex = null;
 
+imageIndexes.forEach(i => {
+  const image = document.createElement('img');
+  image.src = `/images/family_${i}.jpg`;
+})
