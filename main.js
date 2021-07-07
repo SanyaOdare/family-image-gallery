0c0d42e09019de14988392de28d78eb14cc7f79e
Sanya Oluwadare <sanyaodare@gmail.com>
2021-07-07 23:11:33 +0100

    add style

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 const gallery = document.getElementById('gallery');
 const popup = document.getElementById('popup');
 const selectedImage = document.getElementById('selectedImage');
-const imageIndexes = [1,2,3,4,5,6,7,8,9,10];
+const imageIndexes = [1,2,3,4,5,6,7,8,9];
 const selectedIndex = null;
 
 imageIndexes.forEach(i => {
   const image = document.createElement('img');
   image.src = `/images/family_${i}.jpg`;
+  image.alt = `Cover for Family ${i} of the Family Image Gallery`;
+  image.classList.add('galleryImg');
+
+  image.addEventListener('click', () => {
+
+  })
+
+  gallery.appendChild(image);
 })
